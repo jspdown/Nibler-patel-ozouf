85e75db46adfa96b0c122e294a67bb79c3195551
Platel Kevin <kevin.platel@epitech.eu>
2013-03-18 21:42:13 +0100

    Correction NcursesLib

--- inc/NcursesLib.hh
+++ inc/NcursesLib.hh
@@ -1,25 +1,26 @@
 #ifndef __NCURSESLIB_H__
 # define __NCURSESLIB_H__
 
+#include <map>
 #include "ILibrary.hh"
 #include "MainWindow.hh"
 
-class NCursesLibrary : public ILibrary
+class NcursesLibrary : public ILibrary
 {
 private:
-  MainWindow					mWin;
+  ncurses::MainWindow				mWin;
   const Config					*conf;
   Map						*map;
-  std::map<std::string, std::pair<char, int> >	aff;
+  std::map< std::string, std::pair<char, int> >	aff;
 public:
-  NCursesLibrary();
-  virtual ~NCursesLibrary();
+  NcursesLibrary();
+  virtual ~NcursesLibrary();
 
   void		drawRect(Rect* rec);
-  void		init(Config const *config);
-  void		loop(Map const *config);
+  void		init(Config const &config);
+  void		loop(Map const &config);
   void		quit();
-  void		reload(Config const *config);
+  void		reload(Config const &config);
   void		updateEvent(std::stack<std::string> &event);
 };
 
--- src/Library/Ncurses/NcursesLib.cpp
+++ src/Library/Ncurses/NcursesLib.cpp
@@ -5,12 +5,14 @@
 // Login   <kevin platel@epitech.net>
 //
 // Started on  Mon Mar 18 15:54:42 2013 vink
-// Last update Mon Mar 18 17:37:56 2013 kevin platel
+// Last update Mon Mar 18 21:41:49 2013 vink
 //
 
 #include <utility>
 #include <fstream>
 #include <unistd.h>
+
+#include "Trame.hh"
 
 #include "Rect.hh"
 #include "NcursesLib.hh"
@@ -25,12 +27,10 @@ NcursesLibrary::~NcursesLibrary()
 {
 }
 
-void		NcursesLibrary::init(Config const *conf)
+void		NcursesLibrary::init(Config const &conf)
 {
-  this->conf = conf;
-  std::string texture_path = conf->getTexturePath();
-  std::vector<std::string> texture = conf->getTexture();
-  std::fstream;
+  std::string texture_path = conf.getTexturePath();
+  std::vector<std::string> texture = conf.getTexture();
   char	c;
   int	col;
 
@@ -38,23 +38,27 @@ void		NcursesLibrary::init(Config const *conf)
     {
       std::pair<char, int> tmp;
       col = ncurses::MainWindow::getColor(COLOR_RED, COLOR_RED);
-      fstream = std::fstream((texture_path + texture[i]).c_str(), fstream::in);
-      c = fstream.get();
+      std::ifstream file((texture_path + texture[i]).c_str(), std::ios::in);
+      c = file.get();
       tmp.first = c;
-      tmp.col = col;
-      this->aff[texture] = tmp;
-      fstream.close();
+      tmp.second = col;
+      (this->aff)[texture[i]] = tmp;
+      file.close();
     }
 }
 
 void		NcursesLibrary::drawRect(Rect *rec)
 {
   ncurses::Window	*new_win = new ncurses::Window();
-  std::pair<int, int>	&tmp = rec.getPos();
+  std::pair<int, int>	&tmp = rec->getPos();
+  char	str[2];
 
+  str[1] = 0;
   new_win->move(tmp.first, tmp.second);
-  tmp = rec.getSize();
+  tmp = rec->getSize();
   new_win->resize(tmp.first, tmp.second);
+  str[0] = this->aff[rec->getTexture()].first;
+  new_win->print(str, this->aff[rec->getTexture()].second);
   this->mWin.addWindow(new_win);
 }
 
@@ -63,7 +67,7 @@ void		NcursesLibrary::quit()
 
 }
 
-void		NcursesLibray::reload(Config const *config)
+void		NcursesLibrary::reload(Config const &config)
 {
   this->init(config);
 }
@@ -73,22 +77,22 @@ void		NcursesLibrary::updateEvent(std::stack<std::string> &)
 
 }
 
-void		NcursesLibrary::loop(Map const *config)
+void		NcursesLibrary::loop(Map const &map)
 {
-  std::vector< std::list<IEntity*> >&	toAff = config->getElements();
+  std::vector< std::list<IEntity*> >&	toAff = map.getElements();
   char	c[2];
 
   c[1] = 0;
   while ((c[0] = this->mWin.getCh()) != 'q')
     {
-      this->map->getHandleEvent()->emit(Trame::buildTrame(c, -1, std::vector<std::string>, std::vector<std::string>));
-      for (int i = 0; i < toAff.size(); ++i)
+      map.getHandleEvent()->emit(Trame::buildTrame(c, -1, std::vector<std::string>(), std::vector<std::string>()));
+      for (unsigned int i = 0; i < toAff.size(); ++i)
 	{
 	  std::list<IEntity*>::iterator it = toAff[i].begin();
 	  while (it != toAff[i].end())
 	    {
-	      (*it).update();
-	      this->drawRect((*it).getRect());
+	      (*it)->update();
+	      this->drawRect((*it)->getRect());
 	      it++;
 	    }
 	}
@@ -100,6 +104,6 @@ extern "C"
 {
   ILibrary*	create_lib()
   {
-    return new NcursesLib();
+    return new NcursesLibrary();
   }
 }
